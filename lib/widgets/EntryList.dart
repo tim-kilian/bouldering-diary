166ad9c6fbeb7dd03d9caa6a551e06f57ded8b5b
Tim Kilian <mail@timkilian.pl>
2020-01-11 16:23:05 +0100

    Add some custom styling to card

--- lib/widgets/EntryList.dart
+++ lib/widgets/EntryList.dart
@@ -2,22 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EntryList extends StatefulWidget {
-
   @override
   EntryListState createState() => EntryListState();
-
 }
 
 class EntryListState extends State<EntryList> {
-
   final List<String> entries = <String>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Bouldering diary')
-      ),
+      appBar: AppBar(title: Text('Bouldering diary')),
       body: buildList(),
     );
   }
@@ -26,27 +21,26 @@ class EntryListState extends State<EntryList> {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
         itemBuilder: (BuildContext context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final int index = i ~/ 2;
-
-          if (index >= entries.length) {
-
-          }
-
           return buildRow(i.toString());
-        }
-    );
+        });
   }
 
   Widget buildRow(String entry) {
-    return ListTile(
-      title: Text(
-          entry,
+    return Card(
+      child: ListTile(
+        title: Text(
+          'Nordwandhalle',
           style: TextStyle(fontSize: 18),
+        ),
+        subtitle: Text(entry + ' violet 4 orange 1 blue'),
+        trailing: Text(
+          '11.01.2020',
+          style: TextStyle(fontSize: 11),
+        ),
+        onTap: () {
+          print('cow');
+        },
       ),
     );
   }
-
 }
